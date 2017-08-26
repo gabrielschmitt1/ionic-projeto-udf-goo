4dc0aa5b2c43205bbb6f303b2d4200f907a10a64
gabrielschmitt1 <gabrielschmitt7@gmail.com>
2017-08-26 16:03:46 -0300

    Primeiro Commir

---
+++ src/pages/teste/teste.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-teste',
+  templateUrl: 'teste.html'
+})
+export class PageTeste {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+}
